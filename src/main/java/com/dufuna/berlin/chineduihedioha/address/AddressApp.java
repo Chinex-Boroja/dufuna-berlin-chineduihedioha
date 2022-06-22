02425601123c0db092ae39972e784fd4607f46a4
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2022-06-22 14:12:24 +0100

    A simple Hello World program

---
+++ src/main/java/com/dufuna/berlin/chineduihedioha/address/AddressApp.java
@@ -0,0 +1,8 @@
+package com.dufuna.berlin.chineduihedioha.address;
+
+public class AddressApp {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+    }
+}
